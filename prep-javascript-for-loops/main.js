a288d32056eb751e3a3b835ba4afe9b1cf65f109
Alexander Avzaradel <alexanderavzaradel@gmail.com>
2022-08-10 19:56:08 +0000

    Completed prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let g = 0; g < 20; g = g + 2) {
+  console.log(g);
+}
+
+for (let j = 100; j > 0; j--) {
+  const text = 'Time till explosion:' + j;
+  console.log(text);
+}
